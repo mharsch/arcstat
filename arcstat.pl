12384db440cf6219316c41a16caf84748534d375
Mike Harsch <mike@harschsystems.com>
2011-05-12 11:16:28 -0600

    some cstyle fixes

--- arcstat.pl
+++ arcstat.pl
@@ -2,12 +2,12 @@
 #
 # Print out ZFS ARC Statistics exported via kstat(1)
 # For a definition of fields, or usage, use arctstat.pl -v
-# 
+#
 # This script is a fork of the original arcstat.pl (0.1) by
 # Neelakanth Nadgir, originally published on his Sun blog on
 # 09/18/2007
 #     http://blogs.sun.com/realneel/entry/zfs_arc_statistics
-# 
+#
 # This version aims to improve upon the original by adding features
 # and fixing bugs as needed.  This version is maintained by 
 # Mike Harsch and is hosted in a public open source repository:
@@ -17,28 +17,28 @@
 # Contact the maintainer at ( mike at harschsystems dot com )
 #
 # CDDL HEADER START
-# 
+#
 # The contents of this file are subject to the terms of the
 # Common Development and Distribution License, Version 1.0 only
 # (the "License").  You may not use this file except in compliance
 # with the License.
-# 
+#
 # You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
 # or http://www.opensolaris.org/os/licensing.
 # See the License for the specific language governing permissions
 # and limitations under the License.
-# 
+#
 # When distributing Covered Code, include this CDDL HEADER in each
 # file and include the License file at usr/src/OPENSOLARIS.LICENSE.
 # If applicable, add the following below this CDDL HEADER, with the
 # fields enclosed by brackets "[]" replaced with your own identifying
 # information: Portions Copyright [yyyy] [name of copyright owner]
-# 
+#
 # CDDL HEADER END
 #
 #
 # Fields have a fixed width. Every interval, we fill the "v"
-# hash with its corresponding value (v[field]=value) using calculate(). 
+# hash with its corresponding value (v[field]=value) using calculate().
 # @hdr is the array of fields that needs to be printed, so we
 # just iterate over this array and print the values using our pretty printer.
 
@@ -133,11 +133,12 @@ sub init {
 	my $hflag = '';
 	my $vflag;
 	my $res = GetOptions('x' => \$xflag,
-		'o=s' => \$opfile,
-		'help|h|?' => \$hflag,
-		'v' => \$vflag,
-		's=s' => \$sep,
-		'f=s' => \$desired_cols);
+	    'o=s' => \$opfile,
+	    'help|h|?' => \$hflag,
+	    'v' => \$vflag,
+	    's=s' => \$sep,
+	    'f=s' => \$desired_cols);
+
 	$int = $ARGV[0] || $int;
 	$count = $ARGV[1] || $count;
 	usage() if !$res or $hflag or ($xflag and $desired_cols);
@@ -165,13 +166,13 @@ sub init {
 		}
 		if (scalar @invalid > 0) {
 			print STDERR "Invalid column definition! -- "
-				. "@invalid\n\n";
+			    . "@invalid\n\n";
 			usage();
 		}
 
 		if (scalar @incompat > 0) {
 			print STDERR "Incompatible field specified -- "
-				. "@incompat\n\n";
+			    . "@incompat\n\n";
 			usage();
 		}
 	}
@@ -205,7 +206,7 @@ sub snap_stats {
 
 # Pretty print num. Arguments are width, scale, and num
 sub prettynum {
-	my @suffix=(' ','K', 'M', 'G', 'T');
+	my @suffix = (' ', 'K', 'M', 'G', 'T');
 	my $num = $_[2] || 0;
 	my $scale = $_[1];
 	my $sz = $_[0];
@@ -248,7 +249,7 @@ sub print_header {
 }
 
 sub calculate {
-	%v=();
+	%v = ();
 	$v{"time"} = strftime("%H:%M:%S", localtime);
 	$v{"hits"} = $d{"hits"}/$int;
 	$v{"miss"} = $d{"misses"}/$int;
@@ -256,28 +257,28 @@ sub calculate {
 	$v{"hit%"} = 100*$v{"hits"}/$v{"read"} if $v{"read"} > 0;
 	$v{"miss%"} = 100 - $v{"hit%"} if $v{"read"} > 0;
 
-	$v{"dhit"} = ($d{"demand_data_hits"} + 
-		$d{"demand_metadata_hits"})/$int;
-	$v{"dmis"} = ($d{"demand_data_misses"} + 
-		$d{"demand_metadata_misses"})/$int;
+	$v{"dhit"} = ($d{"demand_data_hits"} +
+	    $d{"demand_metadata_hits"})/$int;
+	$v{"dmis"} = ($d{"demand_data_misses"} +
+	    $d{"demand_metadata_misses"})/$int;
 
 	$v{"dread"} = $v{"dhit"} + $v{"dmis"};
 	$v{"dh%"} = 100 * $v{"dhit"}/$v{"dread"} if $v{"dread"} > 0;
 	$v{"dm%"} = 100 - $v{"dh%"} if $v{"dread"} > 0;
 
-	$v{"phit"}=($d{"prefetch_data_hits"} + 
-		$d{"prefetch_metadata_hits"})/$int;
-	$v{"pmis"}=($d{"prefetch_data_misses"}
-		+$d{"prefetch_metadata_misses"})/$int;
+	$v{"phit"} = ($d{"prefetch_data_hits"} +
+	    $d{"prefetch_metadata_hits"})/$int;
+	$v{"pmis"} = ($d{"prefetch_data_misses"} +
+	    $d{"prefetch_metadata_misses"})/$int;
 
 	$v{"pread"} = $v{"phit"} + $v{"pmis"};
 	$v{"ph%"} = 100 * $v{"phit"}/$v{"pread"} if $v{"pread"} > 0;
 	$v{"pm%"} = 100 - $v{"ph%"} if $v{"pread"} > 0;
 
-	$v{"mhit"}=($d{"prefetch_metadata_hits"} + 
+	$v{"mhit"} = ($d{"prefetch_metadata_hits"} +
 		$d{"demand_metadata_hits"})/$int;
-	$v{"mmis"}=($d{"prefetch_metadata_misses"}
-		+$d{"demand_metadata_misses"})/$int;
+	$v{"mmis"} = ($d{"prefetch_metadata_misses"} +
+	    $d{"demand_metadata_misses"})/$int;
 
 	$v{"mread"} = $v{"mhit"} + $v{"mmis"};
 	$v{"mh%"} = 100 * $v{"mhit"}/$v{"mread"} if $v{"mread"} > 0;
@@ -298,7 +299,7 @@ sub calculate {
 		$v{"l2miss"} = $d{"l2_misses"}/$int;
 		$v{"l2read"} = $v{"l2hits"} + $v{"l2miss"};
 		$v{"l2hit%"} = 100 * $v{"l2hits"}/$v{"l2read"} 
-				if $v{"l2read"} > 0;
+		    if $v{"l2read"} > 0;
 
 		$v{"l2miss%"} = 100 - $v{"l2hit%"} if $v{"l2read"} > 0;
 		$v{"l2size"} = $cur{"l2_size"};
